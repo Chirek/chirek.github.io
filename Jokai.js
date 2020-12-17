d6fd0684133b0d7db4c95af83ab3e6e0d10af866
Chirek <76175324+Chirek@users.noreply.github.com>
2020-12-17 17:36:17 +0100

    Add files via upload

---
+++ Jokai.js
@@ -0,0 +1,33 @@
+var a=prompt("Jókai Mór születési helye?\n1,Komárom\n2,Győr\n3,Budapest")
+if(a==1){alert("helyes válasz")}
+else{alert("helytelen válasz")}
+
+
+
+var b=prompt("Gimnáziumi tanulmányait ___________ folytatta.\n1,Debrecenben\n2,Nagyváradon\n3,Komáromban")
+if(b==3){alert("helyes válasz")}
+else{alert("helytelen válasz")}
+
+
+var c=prompt("Jókai barátságot kötött ___________ .\n1,Csokonai Vitéz Mihállyal\n2,Petőfi Sándorral\n3,Berzsenyi Dániellel")
+if(c==2){alert("helyes válasz")}
+else{alert("helytelen válasz")}
+
+
+
+var d=prompt("1845-ben a nyári hónapokat _____________ töltötte.\n1,Szilvásváradon\n2,Balatonfüreden\n3,Komáromban")
+if(d==3){alert("helyes válasz")}
+else{alert("helytelen válasz")}
+
+
+
+
+var d=prompt("Szabadságharc leverése után Jókaiéknak sikerült egy Klapka-féle komáromi ______________ szereznie.\n1,menetlevelet\n2,menlevelet\n3,menetszelet")
+if(d==2){alert("helyes válasz")}
+else{alert("helytelen válasz")}
+
+
+
+var f=prompt("Hol hunyt el Jókai Mór?\n1,Budapest\n2,Nizza\n3,Komárom")
+if(f==1){alert("helyes válasz")}
+else{alert("helytelen válasz")}
